a6dc9889254315ecb89319b36979f619853e34b7
Celyn Khoo <52463140+cllkhoo@users.noreply.github.com>
2020-08-19 20:54:06 +1200

    Update DataExtractionandTrendAnalyses.R

--- DataExtractionandTrendAnalyses.R
+++ DataExtractionandTrendAnalyses.R
@@ -1,81 +1,54 @@
 ---------------------------------------------------EXTRACT AND TIDY CANADIAN HYDROMETRIC DATA------------------------------------------
-
+  
 #Set working directory -- this is a folder/place where all files from this project is saved
-  #Use getwd() to find filepath then enter it within the "" in line 5
-work_directory <- "/Users/celynkhoo/R Projects/Hydrology"
+#Use getwd() to find filepath then enter it within the "" in line 5
+work_directory <- "/Users/celynkhoo/R Projects/Hydrology" 
 setwd(work_directory)
-
+  
 library(tidyhydat)
 library(dplyr) #data manipulation package
 library(ggplot2) #data visualization tool
 library(lubridate) #aids with date and time in R
-
--------------------------------------------------------------GETTING HISTORICAL DATA---------------------------------------------------
+library(scales) #graphical scales for visualization
   
+-------------------------------------------------------------GETTING HISTORICAL DATA---------------------------------------------------
+    
 download_hydat() #downloads HYDAT database, the Canadian National Water Data Archive, data here has been validated/corrected
 NL_stns <- hy_stations(prov_terr_state_loc = "NL") #Finding station information and numbers for NL regional data
-
+  
 #Pulling data from the stations you want. Things you would change in the code following are the station numbers and data type
-Pipers_Hole <- hy_stn_data_range() %>%
-  filter(DATA_TYPE == "Q", STATION_NUMBER == "02ZH001") %>%
+Raw_Data <- hy_stn_data_range() %>%
+  filter(DATA_TYPE == "Q", STATION_NUMBER == "02ZH001") %>% 
   hy_daily_flows()
 
-Come_by_Chance <- hy_stn_data_range() %>%
-  filter(DATA_TYPE == "Q", STATION_NUMBER == "02ZH002") %>%
-  hy_daily_flows()
+write.csv(Raw_Data, "PHR_HistoricalRawData.csv", row.names = FALSE) #Saves Raw_Data dataframe as a csv file. Substitute PHR with desired station name.
 
-Ugjoktok <- hy_stn_data_range() %>%
-  filter(DATA_TYPE == "Q", STATION_NUMBER == "03NF001") %>%
-  hy_daily_flows()
-
-#Station information in list form including name, lat, long, drainage area
-hy_stations(station_number = unique(Pipers_Hole$STATION_NUMBER)) %>%
-  as.list()
-hy_stations(station_number = unique(Come_by_Chance$STATION_NUMBER)) %>%
-  as.list()
-hy_stations(station_number = unique(Ugjoktok$STATION_NUMBER)) %>%
+#Station information in list form including name, lat, long, drainage area. "Raw_Data" in the following code will call 
+#the station specified in line 21-22
+hy_stations(station_number = unique(Raw_Data$STATION_NUMBER)) %>%
   as.list()
 
-#Plotting historical data. You can change the station number and start date for the station you want. 
-his_PipersHole <- hy_daily_flows(station_number = "02ZH001", start_date = "2010-01-01")
-plot(his_PipersHole)
+##Tidying the dataframe for analyses##
+Tidy_Data <- Raw_Data %>%
+  mutate(year = year(Date), month = month(Date), day = day(Date)) %>% #adds columns of year, month and day
+  mutate(Julian = yday(PipersHole_tidy$Date)) %>% #adds a column for Julian Date
+  mutate(Season =
+           ifelse(month %in% c(12,1,2), "Winter", 
+           ifelse(month %in% c(3,4,5), "Spring",
+           ifelse(month %in% c(6,7,8), "Summer",
+           ifelse(month %in% c(9,10,11), "Fall", "Error"))))) #Adds a column for seasonal designation
+#Here, seasons are defined by 'Boreal' hydrology with Winter starting in December to February,
+#Spring starting in March to May, Summer from June to August and Fall from September to November.
 
-#Plotting the time series for the entire record with a snoother added, for other stations: replace 'Pipers_Hole' with new station
-#Picking a time frame for this plot will be better
-Pipers_Hole %>%
-  ggplot(aes(x=Date, y=Value)) +
-  geom_line() + 
-  geom_point() +
-  geom_smooth() +
-  labs(title = "Piper's Hole River", subtitle = "Station Number = 02ZH001", y = "Discharge (m^3/s)") +
-  theme_minimal()
-
-#Normalizing multiple stations by drainage area
-stns <- c("02ZH001", "02ZH002", "03NF001")
-runoff_data <- hy_daily_flows(station_number = stns, start_date = "2018-01-01", end_date = "2018-12-31") %>%
-  left_join(hy_stations(station_number = stns) %>%
-              select(STATION_NUMBER, STATION_NAME, DRAINAGE_AREA_GROSS), by = "STATION_NUMBER") %>%
-  mutate(runoff = Value / DRAINAGE_AREA_GROSS * 86400 / 1e6 *1e3)
-
-ggplot(runoff_data) +
-  geom_line(aes(x=Date, y=runoff, colour = STATION_NAME)) +
-  labs(title = "Normalized Discharge", y="Mean daily runoff (mm/day)", subtitle = "Data Source: Canadian National Water Data Archive") + scale_fill_gradient2() +
-  theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = "bottom", legend.title = element_blank(), legend.direction = "vertical") 
-#Save as PNG 600 x 500
+write.csv(Tidy_Data, "PHR_HistoricalRawDataTidy.csv", row.names = FALSE) #Saves Tidy_Data dataframe as a csv file. Substitute PHR with desired station name.
 
 ----------------------------------------------------------GETTING REALTIME DATA--------------------------------------------------------
 
-#Real-time data has not been validated and may have some missing values  
-  
+#Real-time data aren't been validated and may have some missing values  
 realtime_dd(station_number = "02ZH001") #select specific realtime discharge station
 realtime_plot(station_number = "02ZH001") #plots the most recent month 
-  
-------------------------------------------------MANIPULATING THE DATA FOR TREND ANALYSES----------------------------------------------
 
-#Before we manipulate the data for trend analyses, we will need to change the extracted data with dates that were given in the form of YYYY-MM-DD  
-PipersHole_tidy <- Pipers_Hole %>%
-  mutate(year = year(Date), month = month(Date), day = day(Date)) %>% #adds columns of year, month and day
-  mutate(Julian = yday(PipersHole_tidy$Date)) #adds a column for Julian Date
+------------------------------------------------MANIPULATING THE DATA FOR TREND ANALYSES----------------------------------------------
   
 #GETTING THE 10 YEAR AVERAGE DISCHARGE. Question: Is there any trends in long term annual discharge?
 PipersHole_decadal1 <- PipersHole_tidy %>% 
